03b82bf427cab53bfdd3ae8be7142bad0062da9c
Xplo8E <rvkyadav71@gmail.com>
2024-09-13 14:09:20 +0530

    Initial Commit

---
+++ SecureRequest/SecureRequestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecureRequestApp.swift
+//  SecureRequest
+//
+//  Created by APPLE on 13/09/24.
+//
+
+import SwiftUI
+
+@main
+struct SecureRequestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
